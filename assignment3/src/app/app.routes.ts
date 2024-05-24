ed5f359c7e0e418575566d0a0a261dc9ed98a464
schegga <92823524+schegga@users.noreply.github.com>
2024-05-24 12:43:17 +0200

    Delete assignment3 directory

--- assignment3/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { PersonListComponent } from './person-list/person-list.component';
-
-export const routes: Routes = [
-  { path: '', component: PersonListComponent },
-  // Weitere Routen kannst du hier hinzufügen
-];
